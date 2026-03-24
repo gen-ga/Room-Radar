7f6840ded1dea10d1466502d490001abee38bd6e
gen-ga <kartikeyuniyal2004@gmail.com>
2026-03-24 09:27:35 +0530

    Updated Authentication

--- Authentication.java
+++ Authentication.java
@@ -25,34 +25,3 @@
         System.out.println("Registered!");
     }
 
-    // ---------------- ADMIN ----------------
-    static void adminPanel() {
-        while (true) {
-            System.out.println("\n--- ADMIN PANEL ---");
-            System.out.println("1. Add Room\n2. View Rooms\n3. Delete Room\n4. Logout");
-
-            int ch = sc.nextInt();
-
-            if (ch == 1) {
-                System.out.print("ID: "); int id = sc.nextInt();
-                System.out.print("Location: "); String loc = sc.next();
-                System.out.print("Price: "); double price = sc.nextDouble();
-                System.out.print("Rating: "); double rating = sc.nextDouble();
-
-                rooms.add(new Room(id, loc, price, rating, true));
-                saveRooms();
-                System.out.println("Room added!");
-
-            } else if (ch == 2) {
-                displayRooms(rooms);
-
-            } else if (ch == 3) {
-                System.out.print("Enter ID: ");
-                int id = sc.nextInt();
-                rooms.removeIf(r -> r.id == id);
-                saveRooms();
-                System.out.println("Deleted!");
-
-            } else break;
-        }
-    }
